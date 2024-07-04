76e0e5b6d78df8e35902acd607c3505649ff67dc
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-07-05 04:06:33 +0600

    working on the scalability of routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,44 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import CreateStudent from "../pages/admin/CreateStudent";
+import { AdminPaths } from "./Admin.routes";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App></App>,
-        children: [
-            {
-                path: 'about', //without slash relative path
-                element: <About></About>
-            },
-            {
-                path: 'contact',
-                element: <Contact></Contact>
-            }
-        ]
+        element: <App></App>
     },
     {
         path: '/admin',
         element: <App></App>,
-        children: [
-            {
-                index: true,
-                element: <AdminDashboard></AdminDashboard>
-            },
-            {
-                path: 'dashboard',
-                element: <AdminDashboard></AdminDashboard>
-            },
-            {
-                path: 'create-student',
-                element: <CreateStudent></CreateStudent>
-            }
-        ]
+        children: AdminPaths,
+    },
+    {
+        path: '/faculty',
+        element: <App></App>,
+        children: AdminPaths,
+    },
+    {
+        path: '/student',
+        element: <App></App>,
+        children: AdminPaths,
     },
     {
         path: '/login',
