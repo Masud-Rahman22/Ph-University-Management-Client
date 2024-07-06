6edbb6ba36520aeedea4a9e3f8057c78f12aa959
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-07-06 17:16:40 +0600

    sidebar routes are handled programmatically

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,32 +1,33 @@
 import { Layout, Menu } from "antd";
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { adminSidebarItems } from "../../routes/Admin.routes";
 
 const { Header, Content, Footer, Sider } = Layout;
 
-const items = [
-    {
-        key: 'Dashboard',
-        label: <NavLink to="/admin/dashboard">Dashboard</NavLink>
-    },
-    {
-        key: 'User Management',
-        label: 'User Management',
-        children : [
-            {
-                key: 'Create Admin',
-                label: <NavLink to="/admin/create-admin">Create Admin</NavLink>
-            },
-            {
-                key: 'Create Student',
-                label: <NavLink to="/admin/create-student">Create Student</NavLink>
-            },
-            {
-                key: 'Create Faculty',
-                label: <NavLink to="/admin/create-faculty">Create Faculty</NavLink>
-            },
-        ]
-    },
-]
+// const items = [
+//     {
+//         key: 'Dashboard',
+//         label: <NavLink to="/admin/dashboard">Dashboard</NavLink>
+//     },
+//     {
+//         key: 'User Management',
+//         label: 'User Management',
+//         children : [
+//             {
+//                 key: 'Create Admin',
+//                 label: <NavLink to="/admin/create-admin">Create Admin</NavLink>
+//             },
+//             {
+//                 key: 'Create Student',
+//                 label: <NavLink to="/admin/create-student">Create Student</NavLink>
+//             },
+//             {
+//                 key: 'Create Faculty',
+//                 label: <NavLink to="/admin/create-faculty">Create Faculty</NavLink>
+//             },
+//         ]
+//     },
+// ]
 
 const MainLayout = () => {
     return (
@@ -55,7 +56,7 @@ const MainLayout = () => {
             theme="dark"
             mode="inline"
             defaultSelectedKeys={["4"]}
-            items={items}
+            items={adminSidebarItems}
         />
         </Sider>
         <Layout>
