264240da4b5b1ecdce3654a86d5c1399b5c08118
Masud-Rahman22 <masudrhmn6560@gmail.com>
2024-09-13 02:33:30 +0600

    decoded token and set user in local state

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,26 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/features/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
 
 const Login = () =>{
+  const dispatch = useAppDispatch();
     const {register,handleSubmit} = useForm();
-    const [login, {data,error}] = useLoginMutation()
-    console.log('data =>', data)
-    console.log('error =>', error)
-    const onSubmit = (data)=>{
+    const [login, {error}] = useLoginMutation()
+    const onSubmit = async(data)=>{
         const userInfo = {
             id: data.userId,
             password: data.password
         }
         login(userInfo)
+        const res = await login(userInfo).unwrap();
+        const user = verifyToken(res.data.accessToken)
+        console.log(user)
+        dispatch(setUser({user: user, token: res.data.accessToken}))
     }
+
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
       <div>
