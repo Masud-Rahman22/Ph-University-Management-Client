41f2e8313eedb8b150d3991455d3466a302d4d74
Masud-Rahman22 <masudrhmn6560@gmail.com>
2024-09-12 16:20:11 +0600

    initialized redux with authSlice

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -8,7 +8,6 @@ const MainLayout = () => {
     return (
     <div>
     <Layout style={{ height: "100vh" }}>
-        {/* here sidebar */}
         <Sidebar></Sidebar>
         <Layout>
         <Header style={{ padding: 0 }} />
