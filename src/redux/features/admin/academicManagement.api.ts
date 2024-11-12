17f095d9ea44cf68d6931ad6f7ac1d555526f56b
Masud-Rahman22 <masudrhmn6560@gmail.com>
2024-11-12 11:42:40 +0600

    resolved type error using generic type with a separete semester type

--- src/redux/features/admin/academicManagement.api.ts
+++ src/redux/features/admin/academicManagement.api.ts
@@ -1,4 +1,5 @@
 import { TResponseRedux } from "../../../types";
+import { TAcademicSemester } from "../../../types/academicManagement.type";
 import { baseApi } from "../../api/baseApi";
 
 const academicManagementApi = baseApi.injectEndpoints({
@@ -8,7 +9,7 @@ const academicManagementApi = baseApi.injectEndpoints({
         url: "/academic-semesters",
         method: "GET",
       }),
-      transformResponse: (response:TResponseRedux) => {
+      transformResponse: (response:TResponseRedux<TAcademicSemester[]>) => {
         return {
           data: response.data,
           meta: response.meta
