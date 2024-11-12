63892568bae13d163343b51fa3a43cc9c8d80dcb
Masud-Rahman22 <masudrhmn6560@gmail.com>
2024-11-12 11:20:13 +0600

    created academic semester table data using ant design table

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,12 +1,84 @@
+import { Table, TableColumnsType, TableProps } from "antd";
 import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.api";
-
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
+}
 
 const AcademicSemester = () => {
-    const {data} = useGetAllSemestersQuery(undefined)
-    console.log(data)
+  const { data: semesterData } = useGetAllSemestersQuery(undefined);
+  const tableData = semesterData?.data?.map(
+    ({ _id, name, year, endMonth, startMonth }) => ({
+      _id,
+      name,
+      year,
+      endMonth,
+      startMonth,
+    })
+  );
+  const columns: TableColumnsType<DataType> = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      showSorterTooltip: { target: "full-header" },
+      filters: [
+        {
+          text: "Joe",
+          value: "Joe",
+        },
+        {
+          text: "Jim",
+          value: "Jim",
+        },
+        {
+          text: "Submenu",
+          value: "Submenu",
+          children: [
+            {
+              text: "Green",
+              value: "Green",
+            },
+            {
+              text: "Black",
+              value: "Black",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: "Year",
+      dataIndex: "year",
+    },
+    {
+      title: "Start Month",
+      dataIndex: "startMonth",
+    },
+    {
+      title: "End Month",
+      dataIndex: "endMonth",
+    },
+  ];
+
+  const onChange: TableProps<DataType>["onChange"] = (
+    pagination,
+    filters,
+    sorter,
+    extra
+  ) => {
+    console.log("params", pagination, filters, sorter, extra);
+  };
+
   return (
-    <div>AcademicSemester component</div>
-  )
-}
+    <Table<DataType>
+      columns={columns}
+      dataSource={tableData}
+      onChange={onChange}
+      showSorterTooltip={{ target: "sorter-icon" }}
+    />
+  );
+};
 
 export default AcademicSemester;
