ec443f1fee6e36f88bbcef3aab5c82169d180b19
Masud-Rahman22 <masudrhmn6560@gmail.com>
2024-12-05 01:32:11 +0600

    add faculty modal is added from antD modal

--- src/pages/admin/courseManagement/Courses.tsx
+++ src/pages/admin/courseManagement/Courses.tsx
@@ -33,7 +33,7 @@ const Courses = () => {
     {
       title: 'Action',
       key: 'x',
-      render: (item) => {
+      render: (item: any) => {
         return <AddFacultyModal facultyInfo={item} />;
       },
     },
@@ -71,7 +71,7 @@ const AddFacultyModal = ({ facultyInfo }) => {
     label: item.fullName,
   }));
 
-  const handleSubmit = (data) => {
+  const handleSubmit = (data: any) => {
     const facultyData = {
       courseId: facultyInfo.key,
       data,
